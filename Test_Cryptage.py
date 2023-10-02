2b60d39968d655bd08e478e9d2413f114667a4fe
nabishii <Billalsoda150@gmail.com>
2023-10-02 12:35:52 +0200

    amelioration cryptage

--- Test_Cryptage.py
+++ Test_Cryptage.py
@@ -12,12 +12,6 @@ def crypt(message):
             encrypted_char = char
         encrypted_message += encrypted_char
     return encrypted_message
-
-# Demander à l'utilisateur de saisir un message à crypter
 message = input("Entrez le message à crypter : ")
-
-# Crypter le message
 message_crypte = crypt(message)
-
-# Afficher le message crypté
 print("Message crypté : ", message_crypte)
