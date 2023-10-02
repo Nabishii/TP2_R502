2d852ec418725dc621ae99560157d37d12bef6ab
nabishii <Billalsoda150@gmail.com>
2023-10-02 12:04:11 +0200

    code Test_FizzBuzz.py fonctionnel

---
+++ Test_FizzBuzz.py
@@ -0,0 +1,16 @@
+def affiche():
+    for i in range(1, 101):  
+        output = ""  
+
+        if i % 3 == 0 and i % 5 == 0:  
+            output = "FrisBee"
+        elif i % 3 == 0:  
+            output = "Fizz"
+        elif i % 5 == 0:  
+            output = "Buzz"
+        else:
+            output = str(i)  
+
+        print(output)  
+
+affiche()
