86bbfb704d3fbd1aadbd5464d82d2c6da1062ac1
nabishii <Billalsoda150@gmail.com>
2023-10-02 12:39:13 +0200

    B. Première évolution du code :

--- Test_Cryptage.py
+++ Test_Cryptage.py
@@ -1,17 +1,23 @@
 import string
 
-def crypt(message):
+def crypt(message, pas):
+    if pas < 1 or pas > 9:
+        return "Le pas doit être compris entre 1 et 9."
+
     encrypted_message = ''
     for char in message:
         if char.isalpha():
             if char.islower():
-                encrypted_char = chr(((ord(char) - ord('a') + 1) % 26) + ord('a'))
+                encrypted_char = chr(((ord(char) - ord('a') + pas) % 26) + ord('a'))
             else:
-                encrypted_char = chr(((ord(char) - ord('A') + 1) % 26) + ord('A'))
+                encrypted_char = chr(((ord(char) - ord('A') + pas) % 26) + ord('A'))
         else:
             encrypted_char = char
         encrypted_message += encrypted_char
+
+    encrypted_message += str(pas) 
     return encrypted_message
 message = input("Entrez le message à crypter : ")
-message_crypte = crypt(message)
+pas = int(input("Entrez le pas (entre 1 et 9) : "))
+message_crypte = crypt(message, pas)
 print("Message crypté : ", message_crypte)
